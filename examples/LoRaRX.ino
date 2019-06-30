184d429780ca7bb3e6d22068e737ad17b5ad5368
tinytronix <49196001+tinytronix@users.noreply.github.com>
2019-06-30 18:24:10 +0200

    Add files via upload
    adapted to current SX126x Interface

--- examples/LoRaRX.ino
+++ examples/LoRaRX.ino
@@ -2,10 +2,10 @@
 #include <SX126x.h>
 
 #define RF_FREQUENCY                                433000000 // Hz  center frequency
-#define TX_OUTPUT_POWER                             -3        // dBm tx output power
+#define TX_OUTPUT_POWER                             22        // dBm tx output power
 #define LORA_BANDWIDTH                              4         // bandwidth=125khz  0:250kHZ,1:125kHZ,2:62kHZ,3:20kHZ.... look for radio line 392                                                               
-#define LORA_SPREADING_FACTOR                       7         // spreading factor=11 [SF5..SF12]
-#define LORA_CODINGRATE                             4         // [1: 4/5,
+#define LORA_SPREADING_FACTOR                       7        // spreading factor=11 [SF5..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
                                                               //  2: 4/6,
                                                               //  3: 4/7,
                                                               //  4: 4/8]
@@ -19,51 +19,42 @@
                                                               
 uint8_t* pRxData = new uint8_t[128];
 int16_t rv = 0;
-SX126x  lora(PD5, //SPI select
-             PD6, //reset
-             PD7, //busy
-             PB0  //interrupt DIO1 
-             );
+SX126x  lora;
 
 void setup() 
 {
+  // put your setup code here, to run once:
   Serial.begin(9600);
   
   delay(500);
-  
-  lora.begin(SX126X_PACKET_TYPE_LORA,   //LoRa or FSK, FSK currently not supported
-             RF_FREQUENCY,              //frequency in Hz
-             TX_OUTPUT_POWER);          //tx power in dBm
 
+  lora.begin(SX126X_PACKET_TYPE_LORA,   //LoRa or FSK, FSK currently not supported
+             433000000,                 //frequency in Hz
+             -3);                       //tx power in dBm
+  
   lora.LoRaConfig(LORA_SPREADING_FACTOR, 
-                  LORA_BANDWIDTH, 
-                  LORA_CODINGRATE, 
-                  LORA_PREAMBLE_LENGTH, 
-                  LORA_PAYLOADLENGTH, 
-                  true,             //crcOn  
-                  false             //invertIrq
-                  );
+                    LORA_BANDWIDTH, 
+                    LORA_CODINGRATE, 
+                    LORA_PREAMBLE_LENGTH, 
+                    LORA_PAYLOADLENGTH, 
+                    false,              //crcOn  
+                    false);             //invertIrq
+   
 
 }
 
-//receive and echo back
 void loop() 
 {
-  uint8_t rxLen = lora.Receive(pRxData, 128);
+   uint8_t rxLen = lora.Receive(pRxData, 128);
   if ( rxLen > 0 )
   { 
-    //Serial.println("Receive");
-    lora.Send(pRxData, 1, true);
-      
-    
-    //uint8_t rssi, snr;
-    //Serial.print("rxLen: ");
-    //Serial.println(rxLen, DEC);
-    //Serial.println(pRxData[0], DEC);
-    //lora.ReceiveStatus(&rssi, &snr);
-    //Serial.print("snr: ");
-    //Serial.println(snr, DEC);
-    //Serial.print("rssi: ");
-    //Serial.println(rssi, DEC);
+    uint8_t rssi, snr;
+    Serial.println(pRxData[0], DEC);
+    lora.ReceiveStatus(&rssi, &snr);
+    Serial.print("snr: ");
+    Serial.println(snr, DEC);
+    Serial.print("rssi: ");
+    Serial.println(rssi, DEC);
   }
+  delay(1000);
 }
--- examples/LoRaTX.ino
+++ examples/LoRaTX.ino
@@ -2,10 +2,10 @@
 #include <SX126x.h>
 
 #define RF_FREQUENCY                                433000000 // Hz  center frequency
-#define TX_OUTPUT_POWER                             -3        // dBm tx output power
+#define TX_OUTPUT_POWER                             22        // dBm tx output power
 #define LORA_BANDWIDTH                              4         // bandwidth=125khz  0:250kHZ,1:125kHZ,2:62kHZ,3:20kHZ.... look for radio line 392                                                               
-#define LORA_SPREADING_FACTOR                       7         // spreading factor=11 [SF5..SF12]
-#define LORA_CODINGRATE                             4         // [1: 4/5,
+#define LORA_SPREADING_FACTOR                       7        // spreading factor=11 [SF5..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
                                                               //  2: 4/6,
                                                               //  3: 4/7,
                                                               //  4: 4/8]
@@ -17,71 +17,36 @@
 #define LORA_PAYLOADLENGTH                          0         // 0: variable receive length 
                                                               // 1..255 payloadlength
 
-SX126x  lora(PD5, //SPI select
-             PD6, //reset
-             PD7, //busy
-             PB0  //interrupt DIO1 
-             );
-             
-bool b = false;
+SX126x  lora;
+
 
 void setup() 
 {
+  // put your setup code here, to run once:
   Serial.begin(9600);
   
   delay(500);
-               
+
   lora.begin(SX126X_PACKET_TYPE_LORA,   //LoRa or FSK, FSK currently not supported
-             RF_FREQUENCY,              //frequency in Hz
-             TX_OUTPUT_POWER);          //tx power in dBm
-             
+             433000000,                 //frequency in Hz
+             -3);                       //tx power in dBm
+  
   lora.LoRaConfig(LORA_SPREADING_FACTOR, 
                     LORA_BANDWIDTH, 
                     LORA_CODINGRATE, 
                     LORA_PREAMBLE_LENGTH, 
                     LORA_PAYLOADLENGTH, 
-                    true,              //crcOn  
+                    false,              //crcOn  
                     false);             //invertIrq
    
-  Serial.println("Test starting");
+
 }
 
 uint8_t i;
-uint8_t rxLen = 0;
-uint8_t* pRxData = new uint8_t[128];
-uint32_t errors = 0;
-int count = 0;
-unsigned long start;
 
-//send data and await echo
 void loop() 
 {
-  if ( b == false )
-  {
-    start = millis();
-    lora.Send(&i, 1, true);
-    i++;
-    b = true;
-  }
-  else
-  {
-    rxLen = lora.Receive(pRxData, 128);
-    if ( rxLen > 0 )
-    { 
-      unsigned long roundtrip = millis() - start;
-      Serial.print(pRxData[0], DEC); Serial.print(" "); Serial.print(roundtrip); Serial.println("ms");
-      b = false;
-      count = 0;
-    }
-    else
-    {
-      count++;
-    }
-    if ( count == 500 )
-    {
-      count = 0;
-      Serial.println("Retrigger");
-      b = false;
-    }
-  }
+  lora.Send(&i, 1);
+  i++;
+  delay(1000);
 }
